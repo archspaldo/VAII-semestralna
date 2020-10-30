c1c9f6b76deda066627105e0b43e54e1757b009b
archspaldo <psurin71@gmail.com>
2020-10-30 16:27:12 +0100

    Recreation

--- semestralna/semestralna/settings.py
+++ semestralna/semestralna/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'semestralna.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'user_data',
+        'USER': 'root',
+        'PASSWORD': 'experience',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
