30a70d8946efcffccb9e07051b4284956d7485c1
archspaldo <psurin71@gmail.com>
2020-11-01 21:22:03 +0100

    -

--- semestralna/app/views.py
+++ semestralna/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponseRedirect, request
+from django.urls import reverse
 
 
 class IndexView(View):
@@ -16,12 +17,10 @@ class LoginView(View):
 
 def user_login(request):
     logout(request)
-    meno = ''
-    heslo = ''
     if request.POST:
         pouzivatel = authenticate(
             username=request.POST['meno'], password=request.POST['heslo'])
         if pouzivatel is not None and pouzivatel.is_active:
             login(request, pouzivatel)
-            return HttpResponseRedirect('app/index.html')
-    return render(request, 'app/login.html', {'error': 'Meno alebo heslo sú nesprávne'})
+            return HttpResponseRedirect(reverse(''))
+    return render(request, 'app/login.html', {'error': 'Meno alebo heslo sú nesprávne', })
