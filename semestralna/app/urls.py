02d950dd8f0ac0c6329a522ad275234ce5554880
archspaldo <psurin71@gmail.com>
2020-11-02 19:38:34 +0100

    redone login

--- semestralna/app/urls.py
+++ semestralna/app/urls.py
@@ -5,5 +5,6 @@ app_name = 'app'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('login', views.LoginView.as_view(), name='login'),
-    path('user_login', views.user_login, name='user_login')
+    path('user_login', views.user_login, name='user_login'),
+    path('user_logout', views.user_logout, name='user_logout'),
 ]
--- semestralna/app/views.py
+++ semestralna/app/views.py
@@ -4,6 +4,21 @@ from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponseRedirect, request
 from django.urls import reverse
 
+context = {}
+
+
+def get_context(key):
+    global context
+    value = context.get(key)
+    if value or value == {}:
+        context[key] = {}
+    return value
+
+
+def set_context(key, value):
+    global context
+    context[key] = value
+
 
 class IndexView(View):
     def get(self, request):
@@ -12,7 +27,10 @@ class IndexView(View):
 
 class LoginView(View):
     def get(self, request):
-        return render(request, 'app/login.html')
+        if request.user.is_authenticated:
+            return render(request, 'app/login_succ.html')
+        else:
+            return render(request, 'app/login.html', get_context('login'))
 
 
 def user_login(request):
@@ -20,12 +38,13 @@ def user_login(request):
     if request.POST:
         pouzivatel = authenticate(
             username=request.POST['meno'], password=request.POST['heslo'])
-        if pouzivatel is not None and pouzivatel.is_active:
+        if pouzivatel is not None:
             login(request, pouzivatel)
-            return render(request, 'app/confirmation.html', {'message': 'Úspešne prihlasený'})
-    return render(request, 'app/login.html', {'error': 'Meno alebo heslo sú nesprávne', })
+            return HttpResponseRedirect(reverse('app:login'))
+    set_context('login', {'error': 'Meno alebo heslo sú nesprávne', })
+    return HttpResponseRedirect(reverse('app:login'))
 
 
 def user_logout(request):
     logout(request)
-    return render(request, 'app/confirmation.html', {'message': 'Úspešne odhlasený'})
+    return HttpResponseRedirect(reverse('app:login'))
