b337651e86c4bc46d53e5c4b59ab80d059c7356d
archspaldo <psurin71@gmail.com>
2020-11-08 11:28:51 +0100

    Created custom user

--- semestralna/semestralna/settings.py
+++ semestralna/semestralna/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # custom
-    'app.apps.AppConfig'
+    'app.apps.AppConfig',
+    'Accounts'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'Accounts.User'
